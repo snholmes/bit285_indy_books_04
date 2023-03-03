ae52e019d55aac7691419bb260c6376e90b1885a
Shelby Holmes <shelbynholmes@gmail.com>
2023-03-02 19:06:40 -0800

    Ran into error "Unable to cast object of type 'System.Int32 to type 'System.Int64' on line 20 of SearchResults.cshtml (@for each (var book in Model)

--- Controllers/WriterAPIController.cs
+++ Controllers/WriterAPIController.cs
@@ -15,7 +15,7 @@ namespace IndyBooks.Controllers
         public WriterAPIController(IndyBooksDbContext db) { _db = db; }
 
         /**
-         * READ: Retrievs a collection of writers
+         * READ ALL: Retrievs a collection of writers
          * uses a GET verb with the URL pattern "api/writers"
          */
         [HttpGet]
@@ -31,10 +31,12 @@ namespace IndyBooks.Controllers
         public ActionResult Delete(long id)
         {
             //TODO: Search for record using Any(); if missing -> return NotFound();
-            if (!_db.Writers.Any(/* lambda expression */)) { return NotFound(); }
+            if (!_db.Writers.Any(w => w.Id == id /* lambda expression */)) { return NotFound(); }
 
             //TODO: Make changes to DbContext, save to Database -> return Accepted();
-
+            
+            _db.Writers.Remove(new Writer { Id = id });
+            _db.SaveChanges();
             return Accepted();
         }
         /**
@@ -45,25 +47,31 @@ namespace IndyBooks.Controllers
         public IActionResult Post([FromBody]Writer writer)
         {
             //TODO: Test for an invalid ModelState -> return BadRequest();
-
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
 
             //TODO: Make changes to DbContext, save to Database -> return Accepted(writer);
-
+            _db.Writers.Add(writer);
+            _db.SaveChanges();
             return Accepted(writer);
 
         }
-        /**
-         * READ: Retrieves a particular writer with the given {id}
+        /*
+         * READ 1: Retrieves a particular writer with the given {id}
          * uses a GET verb with the URL pattern "api/writers/41"
          */
-        //[HttpGet("{id}")] //TODO: uncomment this annotation and create the method
+        [HttpGet("{id}")] //TODO: uncomment this annotation and create the method
+        public IActionResult Get(long id)
+        {
+            //TODO: Test for missing record using Any() -> return NotFound();
+            if (!_db.Writers.Any(w => w.Id == id /* lambda expression */)) { return NotFound(); }
 
-        //{
-        //TODO: Test for missing record using Any() -> return NotFound();
 
-
-        //TODO: Make changes to DbContext, save to Database -> return Ok(writer);
-        //}
+            //TODO: Make changes to DbContext, save to Database -> return Ok(writer);
+            return Ok(new Writer { Id = id });
+        }
 
         /** 
          * UPDATE: Modify a given writer with id and [FromBody]Writer writer parameters
---
+++ Migrations/20230302223136_initial.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IndyBooks.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Writers",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Writers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    SKU = table.Column<string>(type: "TEXT", nullable: true),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false),
+                    AuthorId = table.Column<long>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Books_Writers_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Writers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_AuthorId",
+                table: "Books",
+                column: "AuthorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Writers");
+        }
+    }
+}
